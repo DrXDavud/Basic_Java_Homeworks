cac6571458a57d180df71470d8d7648d8fc9b362
Davud <133903533+DrXDavud@users.noreply.github.com>
2023-06-10 12:38:34 +0200

    Add files via upload

---
+++ Basic_Java_Homework2/src/main/java/Homework2.java
@@ -0,0 +1,16 @@
+public class Homework2 {
+    public static void main(String[] args) {
+
+        //1.
+        double x = (float) Math.pow(Math.E, 5) + 1.5;
+        System.out.println(x);
+
+        //2.
+        int d = (byte) Math.log(2) + (short) 14;
+        System.out.println(d);
+
+        //3.
+        double x2 = 1 + 1.5;
+        System.out.println(x2);
+    }
+}
