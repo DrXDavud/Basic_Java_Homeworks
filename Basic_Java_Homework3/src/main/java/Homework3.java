a93176d5443f8cffb0ca23574169b827e03da3da
Davud <133903533+DrXDavud@users.noreply.github.com>
2023-06-14 17:28:30 +0200

    Add files via upload

---
+++ Basic_Java_Homework3/src/main/java/Homework3.java
@@ -0,0 +1,51 @@
+public class Homework3 {
+    public static void main(String[] args) {
+
+        char d = '0';
+        char c = '0';
+        char a = '0';
+
+        //  1st method
+        int v = d;
+        System.out.println(v);
+
+        // 2nd method
+        String s = String.valueOf(c);
+        int j = Integer.parseInt(s);
+        System.out.println(j);
+
+        //3rd method
+        int k = Character.getNumericValue(a);
+        System.out.println(k);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
