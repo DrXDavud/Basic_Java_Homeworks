9ac230ce69732ed765bfccd7f925336eedd5be0e
Davud <133903533+DrXDavud@users.noreply.github.com>
2023-07-02 10:14:11 +0200

    Add files via upload

---
+++ Basic_Java_Homework8/src/main/java/CycleHomework.java
@@ -0,0 +1,10 @@
+public class CycleHomework {
+    public static void main(String[] args) {
+
+        for (int z = 1; z < 513; z = z * 2){
+            System.out.print(z + " ");
+
+        }
+
+    }
+}
